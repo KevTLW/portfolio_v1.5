080cbb6108f070e1767dd6cecdb9eed1b8e7b808
KevTLW <kevinmcruzc@gmail.com>
2020-01-12 23:05:36 -0500

    Implemented 404 error page

---
+++ src/pages/404.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styles from '../styles/404.module.css';
+
+export const data = graphql`
+  query {
+    logo: file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fixed(width: 150, height: 150) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
+
+const NotFound = ({ data, location }) => {
+  const back = () => {
+    window.history.back();
+  }
+  
+  return (
+    <main className={styles.container}>
+      <Img alt='KevTLW logo' className={styles.logo} fixed={data.logo.childImageSharp.fixed} />
+      <h1 className={styles.title}>404 error</h1>
+      <h2 className={styles.text}>the page kevtlw.com{location.pathname} was not found</h2>
+      <p className={styles.text}>go <button className={styles.link} onClick={back}>back</button> or head over to <a className={styles.link} href='https://kevtlw.com'>kevtlw.com</a></p>
+    </main>
+  );
+}
+
+export default NotFound;
